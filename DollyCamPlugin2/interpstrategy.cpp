1ecaf05ab779cb28a63e92e85fedbc4f7c2dfec3
Chris <chris.m@live.nl>
2017-11-12 15:39:26 +0100

    fixed rotation for bezier curves

--- DollyCamPlugin2/interpstrategy.cpp
+++ DollyCamPlugin2/interpstrategy.cpp
@@ -74,13 +74,15 @@ NewPOV NBezierInterpStrategy::GetPOV(float gameTime, int latestFrame)
 	Vector v;
 	float fov = 0;
 	int n = camPath->size();
-ran-d	int k = 0;
-	for (savetype::const_iterator it = camPath->cbegin(); it != camPath->cend(); it++)
+	int k = 0;
+	Rotator lastRotator = camPath->begin()->second.rotation.ToRotator();
+	Rotator addedRotator = { -16364 + 16340, -32768 + 32764, -32768 + 32764 };
+	for (savetype::iterator it = camPath->begin(); it != camPath->end(); it++)
 	{
 		float weight = it->second.weight;
 
 
-		uint64_t fact = (factorial(n - 1) / ((factorial(k)*factorial((n - 1) - k)))); //Maximum of 19 points =\ 
+		uint64_t fact = (factorial(n - 1) / ((factorial(k)*factorial((n - 1) - k)))); //Maximum of 19 points =\  
 		float po = pow(1 - t, n - 1 - k) * pow(t, k);
 		float pofact = po * fact;
 
@@ -91,13 +93,24 @@ ran-d	int k = 0;
 		//pofact *= weight;
 
 		Vector v2 = Vector(pofact) * it->second.location;
-		CustomRotator r2 = it->second.rotation * pofact;
+		
 		
 		float fov2 = pofact * (weight * it->second.FOV);
-
-		
+		Rotator usedRotator;
+		if (it != camPath->begin()) {
+			Rotator rotDiff = CustomRotator(lastRotator).diffTo(it->second.rotation).ToRotator();
+			usedRotator = lastRotator + rotDiff;
+		}
+		else
+		{
+			usedRotator = it->second.rotation.ToRotator();
+		}
+		Rotator r2 = { usedRotator.Pitch * pofact, usedRotator.Yaw * pofact, usedRotator.Roll * pofact };
+		lastRotator = usedRotator;
 		v = v + v2;
-		rot = rot + r2;
+		rot.Pitch += r2.Pitch;
+		rot.Yaw += r2.Yaw;
+		rot.Roll += r2.Roll;
 		//rot += r2;
 
 		fov = fov + fov2;
